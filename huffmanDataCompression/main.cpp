365c0a198715aef19d9fa16306c22330901be6e6
Sathyasai <eeenginmaster95@gmail.com>
2022-12-15 21:36:06 +0800

    Initial Commit

---
+++ huffmanDataCompression/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  huffmanDataCompression
+//
+//  Created by Sart Way on 15/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
